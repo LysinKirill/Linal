5de919592ac887d605079b8768188d3e50a12c34
Lysin Kirill <lysinkirill@gmail.com>
2023-05-17 22:25:09 +0300

    exception

--- Linal/RazrabDaunException.cs
+++ Linal/RazrabDaunException.cs
@@ -2,7 +2,7 @@
 
 public class RazrabDaunException : Exception
 {
-    public RazrabDaunException() : base("The creator of this program is a moron!") {}
+    public RazrabDaunException() : base("Razrab daun!!!") {}
 
-    public RazrabDaunException(string message) : base(message + "\nKeep in mind: the one who wrote this piece of junk is a moron!") {}
+    public RazrabDaunException(string message) : base(message) {}
 }
